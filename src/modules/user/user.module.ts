74d6aee5f119241cea89a3fad961e648a6502c96
njrapidinnovation <rinj@rapidinnovation.dev>
2021-02-02 15:45:37 +0530

    jwt iniitialize

--- src/modules/user/user.module.ts
+++ src/modules/user/user.module.ts
@@ -3,13 +3,20 @@ import { UserService } from './user.service';
 import { UserController } from './user.controller';
 import { userNonceRepository, UserRepository } from './user.repository';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { JwtModule } from '@nestjs/jwt';
 
 @Module({
   imports:[
     TypeOrmModule.forFeature([
       UserRepository,
       userNonceRepository
-    ])],
+    ]),JwtModule.register({
+    
+      secret: "hello",
+    
+      signOptions:{ expiresIn:604800, },
+   
+    })],
   providers: [UserService],
   controllers: [UserController],
   exports:[UserService]
